c176c498c3b54dec2d656dee45450f8cddab8150
Peter Kaminski <kaminski@istori.com>
2019-01-04 14:37:04 -0800

    add associated tables

--- omat.py
+++ omat.py
@@ -18,6 +18,7 @@ from airtable import Airtable # pip install airtable-python-wrapper
 def init_argparse():
     parser = argparse.ArgumentParser(description='Load OMDb metadata into an Airtable base.')
     parser.add_argument('--movies', required=True, help='path to a text file containing names of movies, one per line')
+    parser.add_argument('--include-adult', action='store_true', help='include movies with genre "Adult"')
     return parser
 
 # Load movie names
@@ -37,27 +38,39 @@ def insert_movie(movies_table, data):
     movies_table.insert(data)
 
 # Get movie metadata given movie ID
-def get_movie(movie_id):
+def get_movie_data(movie_id):
     return omdb.imdbid(movie_id)
 
 # safe integer
 def safe_int(string):
     string = string.replace('$','').replace(',','').replace('N/A','').replace(' min','')
-    int(string) if string != '' else 0
+    return int(string) if string != '' else 0
 
 # safe float
 def safe_float(string):
     string = string.replace('$','').replace(',','').replace('N/A','')
-    float(string) if string != '' else 0
+    return float(string) if string != '' else 0
+
+# get record IDs from an associated table given a string of names
+# create new records if necessary
+def get_multiple_records(table, field, names):
+    records = {}
+    for name in names.split(', '):
+        records[name] = table.match(field, name)
+        if len(records[name]) == 0:
+            records[name] = table.insert({field: name})
+    return [records[name]['id'] for name in records]
 
 # Transform metadata from OMDb to Airtable columns
 def transform_data(data):
     return {
-        'Title': data['title'],
+        'Title': "{} ({})".format(data['title'], data['year']),
+        'Directors': data['directors'],
+        'Actors': data['actors'],
         'Year': int(data['year']),
         'Rated': data['rated'],
         'Runtime': safe_int(data['runtime'].replace(' min','')),
-        'Genre': data['genre'],
+        'Genres': data['genres'],
         'Plot': data['plot'],
         'Poster': [{'url': data['poster']}],
         'Metascore': safe_int(data['metascore']),
@@ -66,8 +79,9 @@ def transform_data(data):
         'IMDB URL': 'https://www.imdb.com/title/{}/'.format(data['imdb_id']),
         'Box Office': safe_int(data['box_office']),
         'Production': data['production'],
-        'OMDb Metadata': True,
-        'To Verify': True }
+        'OmAtData': True,
+        'Verified': False,
+        'New': True }
 
 def main():
     # get auth keys from environment
@@ -86,6 +100,7 @@ def main():
     movies_table = Airtable(airtable_base_movies, 'Movies', api_key=airtable_api_key)
     directors_table = Airtable(airtable_base_movies, 'Directors', api_key=airtable_api_key)
     actors_table = Airtable(airtable_base_movies, 'Actors', api_key=airtable_api_key)
+    genres_table = Airtable(airtable_base_movies, 'Genres', api_key=airtable_api_key)
 
     # read movie names, insert movies
     for movie_name in load_movie_names(args.movies):
@@ -93,7 +108,13 @@ def main():
         movies = get_movies(movie_name.rstrip())
         for movie in movies['included']:
             print(' ', movie['title'])
-            data_transformed = transform_data(get_movie(movie['imdb_id']))
+            movie_data = get_movie_data(movie['imdb_id'])
+            if movie_data['genre'] == 'Adult' and not args.include_adult:
+                continue
+            movie_data['directors'] = get_multiple_records(directors_table, 'Name', movie_data['director'])
+            movie_data['actors'] = get_multiple_records(actors_table, 'Name', movie_data['actors'])
+            movie_data['genres'] = get_multiple_records(genres_table, 'Name', movie_data['genre'])
+            data_transformed = transform_data(movie_data)
             insert_movie(movies_table, data_transformed)
 
 # Run this script
